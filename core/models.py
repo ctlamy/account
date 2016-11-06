73fe1370e63bce3a08bcd484a7010088e7df2b9d
ctlamy <christophe.t.lamy@gmail.com>
2016-11-05 18:59:49 -0700

    create entry form + html page

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+	category_choices = (('S','Sale'),('COGS','CostOfGoodsSold'),('Sal','Salaries'),('R','Rent'))
+	se_choices = (('S','Sale'),('E','Expense'))
+
+	se = models.CharField(max_length=1,choices=se_choices)
+	category = models.CharField(max_length=16,choices=category_choices)
+	name = models.CharField(max_length=16)
+	date = models.DateField()
+	desc = models.CharField(max_length=128)
+	amount = models.DecimalField(max_digits=19,decimal_places=2)
--- core/views.py
+++ core/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.views.generic.edit import CreateView
+from core.models import Entry
+
+class CreateEntry(CreateView):
+	template_name = 'entry_input.html'
+	
+	model = Entry
+	fields = ['se', 'category', 'name', 'date', 'desc', 'amount']
